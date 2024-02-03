50e905df9ab116d149badb526f06d08071659fc3
Somnath <107599122+SomnathRoy-JU25@users.noreply.github.com>
2024-02-03 15:16:05 +0530

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-app-backend-3y41.onrender.com/";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
